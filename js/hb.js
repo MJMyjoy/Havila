75226b112a39eed9fd2fb8dc08fa01607544c9d6
Myjoy <20mk321@esisalama.org>
2023-05-13 10:16:19 +0200

    mises a jour hb

--- js/hb.js
+++ js/hb.js
@@ -10,19 +10,26 @@ days[0] = new Date("07/07/2023");
 days[1] = new Date("07/07/2023");
 days[2] = new Date("07/16/2023");
 days[3] = new Date("08/04/2023");
-// days[4] = new Date("00/00/2024"); ...
+days[4] = new Date("10/18/2023");
+days[5] = new Date("11/02/2023");
+days[6] = new Date("11/19/2023");
+// days[7] = new Date("01/01/2024");
 
 
 noms[0].innerHTML = "Chrinovic MUKEBA";
 noms[1].innerHTML = "Christine KABEMBO";
 noms[2].innerHTML = "Ronny NKOYI";
 noms[3].innerHTML = "Sharon-Rose ILUNGA";
-// noms[4].innerHTML = "..."; ...
+noms[4].innerHTML = "Ruth LUMBALA";
+noms[5].innerHTML = "Harmonie MUJINGA";
+noms[6].innerHTML = "Rebecca ESSEDI";
+noms[7].innerHTML = "En attente...";
+// noms[8].innerHTML = "En attente...";
 
 
 
 
-for(let i=0; i<5; i++)
+for(let i=0; i<jourjs.length; i++)
 {
     /*Ici j’utilise les ternaires pour n’afficher la valeur exacte
     du jourj que si elle est supérieur à zéro */
